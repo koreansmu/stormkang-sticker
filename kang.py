d52419c22225dbfb5e928220e62cd02a7e95d74b
koreansmu <shuklakusum4q@gmail.com>
2025-01-25 00:01:03 +0530

    Update kang.py

--- kang.py
+++ kang.py
@@ -12,7 +12,7 @@ import logging
 import json
 
 from typing import Optional, List
-from telegram.constants import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import TelegramError
 from telegram import Update, Bot
 from telegram.ext import CommandHandler, run_async, Updater, Handler, InlineQueryHandler
